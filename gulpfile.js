fb2bb99c69066151c86221321bf144c6c45142c7
Rodrigo <rodrigovieira18@gmail.com>
2015-08-31 14:02:03 -0300

    Adding favicon

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,3 @@ gulp.task('default',function(cb){
   spawn('node_modules/hexo/bin/hexo', ['serve'], { stdio: 'inherit' });
   spawn('gulp', ['--cwd', 'themes/main/'], { stdio: 'inherit' });
 });
-
-gulp.task('build',function(cb){
-  spawn('gulp', ['--cwd', 'themes/main/', 'build'], { stdio: 'inherit' });
-  shell.task(['node_modules/hexo/bin/hexo generate'])
-});
