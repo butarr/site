9454dcc6815176f17e882193c45e53fed4337331
Will Mendes <willmendesneto@gmail.com>
2015-09-02 17:06:26 -0300

    Resolve for local package in gulpfile task

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var gulp = require('gulp'),
     shell = require('gulp-shell'),
     spawn = require('child_process').spawn;
 
-gulp.task('default',function(cb){
-  spawn('node_modules/hexo/bin/hexo', ['serve'], { stdio: 'inherit' });
+gulp.task('default', function(cb){
+  spawn('./node_modules/hexo/bin/hexo', ['serve'], { stdio: 'inherit' });
   spawn('gulp', ['--cwd', 'themes/main/'], { stdio: 'inherit' });
 });
