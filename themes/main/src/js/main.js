6e8d3a16f88571367e168d0743c33791184ea586
Will Mendes <willmendesneto@gmail.com>
2015-09-03 02:22:31 -0300

    Add new component: "social-network"

--- themes/main/src/js/main.js
+++ themes/main/src/js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  $(".menu-options").click(function() {
-    var toggle_el = $(this).data("toggle");
-    $(toggle_el).toggleClass("open");
-    $(this).toggleClass("open");
+  $('.menu-options').click(function() {
+    var toggle_el = $(this).data('toggle');
+    $(toggle_el).toggleClass('open');
+    $(this).toggleClass('open');
   });
+
+  if ($('.social-network')[0]) {
+    $(window).scroll(function(){
+      if( $(window).scrollTop() > 40) {
+        $('.social-network').css('margin-top', '-180px');
+      } else {
+        $('.social-network').css('margin-top', '90px');
+      }
+    });
+    
+    $(window).trigger('scroll');
+  }
+
 });
