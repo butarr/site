572d6e446ecbf42ecd2a822ad725b706ffebab60
Rodrigo <rodrigovieira18@gmail.com>
2015-09-07 10:22:10 -0300

    Getting last news information from JSON.

---
+++ scripts/last_news.js
@@ -0,0 +1,28 @@
+var jsonFiles = function(locals){
+  var posts = locals.posts.toArray();
+
+  var first_posts =
+    posts.sort(function(a, b){
+      return b.updated - a.updated;
+    })
+    .slice(0, 10 )
+    .map(function(element){
+      console.log(element.path);
+      return {
+        title: element.title,
+        hat:   element.hat,
+        description: element.description,
+        cover: element.cover,
+        place: element.place,
+        url: element.permalink,
+        date: element.date.format()
+      };
+    });
+
+  return {
+    path: 'json/last-news.json',
+    data: JSON.stringify(first_posts)
+  };
+};
+
+hexo.extend.generator.register('last-news', jsonFiles);
---
+++ themes/main/src/js/post/last_news.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  var loadLastNews = function(data){
+    var posts  = data || [{}];
+
+    var html = '';
+
+    posts.forEach(function(post){
+      var section = '<section class="news">' +
+                      '<a href="//'+post.url+'" class="thumbnail-link" title="Link para a noticia recente" >'+
+                        '<img src="'+post.cover['small']+'" alt="'+post.cover['subtitle']+'"/>'+
+                      '</a>'+
+                      '<a href="//'+post.url+'" class="lead-link" title="Link para a noticia recente">'+
+                        '<span class="hat">'+post.hat+'</span>'+
+                        '<h2 class="description">'+post.title+'</h2>'+
+                      '</a>'+
+                    '</section>';
+
+
+      html += section;
+    });
+
+    $('#last-news').append(html);
+  };
+
+  $.getJSON(CONFIG.last_news_address, loadLastNews);
+});
