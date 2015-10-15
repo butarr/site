9296cffdfc6b44f0afbadd8a3206916c1bcdbfc5
Rodrigo <rodrigovieira18@gmail.com>
2015-10-15 14:23:41 -0300

    #99 Get dinamically markdown information from Columnists

--- scripts/columnist.js
+++ scripts/columnist.js
@@ -6,21 +6,23 @@ var slugize = util.slugize;
 
 
 var columnists =  function(locals){
-  var config = this.config;
-  var archiveDir = config.archive_dir;
   var allColumnists = locals.data.columnists || [];
   var result = [];
 
   if (_.size(allColumnists) === 0) return;
 
+  function getPostsByColumnist(columnist){
+    return locals.posts.find({'columnist': columnist});
+  }
+
   function generate(columnists){
-    _.each(columnists, function(element){
+    _.each(columnists, function(element, index){
       result.push({
           layout: 'columnist',
           path: slugize(element.name, {transform: 1})+'/',
           data: {
             columnist: element,
-            posts: [1,2,3]
+            posts: getPostsByColumnist(index)
           }
       });
     });
