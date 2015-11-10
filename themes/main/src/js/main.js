4e7b879bf069cf32f48cd9d57b10a57d51fa5a0e
wandecleya <wandecleya@gmail.com>
2015-11-10 17:40:23 -0300

    #36 - Adds scroll behavior to menu and banner

--- themes/main/src/js/main.js
+++ themes/main/src/js/main.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+
+  $(window).scroll(function(){
+    var top = $(this).scrollTop();
+    if ( top > 120 ){
+      $('.banner-header').addClass('smaller-banner');
+      $('.menu-options').addClass('smaller-menu');
+      $('.menu-nav').addClass('smaller-nav');
+    }
+    else{
+      $('.banner-header').removeClass('smaller-banner');
+      $('.menu-options').removeClass('smaller-menu');
+      $('.menu-nav').removeClass('smaller-nav');
+    }
+  });
+
   $('.menu-options').click(function() {
     var toggle_el = $(this).data('toggle');
     $(toggle_el).toggleClass('open');
