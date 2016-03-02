53f5d26ac7ac5f25360603e5192b9071df1bc273
Michael Feitosa <mkaelzinho@gmail.com>
2016-03-02 14:58:34 -0300

    Add node-reset-scss to reset defaults browsers CSS

--- themes/main/gulp/sass.js
+++ themes/main/gulp/sass.js
@@ -1,15 +1,16 @@
-var gulp      = require('gulp'),
-	plumber     = require('gulp-plumber'),
-  sass        = require('gulp-sass'),
-	concat      = require('gulp-concat'),
-  bourbon     = require('node-bourbon').includePaths;
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var bourbon = require('node-bourbon').includePaths;
+var nodeResetScss = require('node-reset-scss').includePath;
 
 module.exports =  function (baseDir) {
 	return function() {
 	  return gulp.src(['src/sass/**/*.scss', '!src/sass/_include/'])
 			.pipe(plumber())
 	    .pipe(sass({
-	        includePaths: ['styles'].concat(bourbon)
+	        includePaths: [nodeResetScss, 'styles'].concat(bourbon)
 	    }).on('error', sass.logError))
 			.pipe(concat('main.css'))
 			.pipe(gulp.dest(baseDir+'/css/'));
