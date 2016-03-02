535069fa35c40e172db7dfb19a2c8ef073e18075
Michael Feitosa <mkaelzinho@gmail.com>
2016-03-02 15:23:47 -0300

    Remove bourboun from project

--- themes/main/gulp/sass.js
+++ themes/main/gulp/sass.js
@@ -2,7 +2,6 @@ var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
-var bourbon = require('node-bourbon').includePaths;
 var nodeResetScss = require('node-reset-scss').includePath;
 
 module.exports =  function (baseDir) {
@@ -10,7 +9,7 @@ module.exports =  function (baseDir) {
 	  return gulp.src(['src/sass/**/*.scss', '!src/sass/_include/'])
 			.pipe(plumber())
 	    .pipe(sass({
-	        includePaths: [nodeResetScss, 'styles'].concat(bourbon)
+	        includePaths: [nodeResetScss, 'styles']
 	    }).on('error', sass.logError))
 			.pipe(concat('main.css'))
 			.pipe(gulp.dest(baseDir+'/css/'));
