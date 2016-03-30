e8f08553da1385b2db35ddc6ba5147f35ac9df8f
Michael Feitosa <mkaelzinho@gmail.com>
2016-03-30 13:31:27 -0300

    Exit when hexo init fails

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var Hexo = require('hexo');
 var hexo = new Hexo(process.cwd(), {'debug': true});
 
-hexo.init().then(function() {
-  hexo.call('generate', {'watch': true})
-    .then(function() {
-      console.log('Hexo is watching...');
-      return hexo.exit();
-    }).catch(function(err) {
-      console.error('Hexo failed.', err);
-      return hexo.exit(err);
-    });;
-});
+hexo.init()
+  .then(function() {
+    hexo.call('generate', {'watch': true})
+      .then(function() {
+        return hexo.exit();
+      })
+      .catch(function(err) {
+        console.error('Hexo generate failed.', err);
+        return hexo.exit(err);
+      });
+  })
+  .catch(function(err) {
+    console.error('Hexo init failed.', err);
+    return hexo.exit(err);
+  });
