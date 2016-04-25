96dc9a58a2f6332d4ca54251c628d7ce4bbdfc96
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-04-25 19:57:18 -0300

    Revert "[Patrick] - #428: introduces custom_format_to_moment helper"
    This reverts commit 78c9463ea0fcfd3d818bb37807d9aa4e23b6d2e2.

--- themes/main/scripts/custom_format_to_moment.js
+++
@@ -1,9 +0,0 @@
-var moment = require('moment-timezone');
-
-function customFormatToMoment(isoUtcDateWithNumberSigns) {
-  var isoUtcDate = isoUtcDateWithNumberSigns.replace('####', '');
-
-  return moment(isoUtcDate).tz(this.config.timezone);
-}
-
-hexo.extend.helper.register('custom_format_to_moment', customFormatToMoment);
