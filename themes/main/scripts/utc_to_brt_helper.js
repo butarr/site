c70fc6d0ecd208c65b487c994ace2c82ffd9ef24
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-04-28 14:24:21 +0000

    [Patrick] - #428: it seems that moments needs to be constructed from a String in order to timezone conversions work

--- themes/main/scripts/utc_to_brt_helper.js
+++ themes/main/scripts/utc_to_brt_helper.js
@@ -1,7 +1,14 @@
 var moment = require('moment-timezone');
 
 function utcToBrt(utcDate) {
-  return moment(utcDate).tz('America/Sao_Paulo');
+  var date;
+  if(typeof utcDate === 'string') {
+    date = moment(utcDate);
+  } else {
+    date = moment(utcDate.toISOString());
+  }
+
+  return date.tz('America/Sao_Paulo');
 }
 
 hexo.extend.helper.register('utc_to_brt', utcToBrt);
