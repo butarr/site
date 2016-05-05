747481d40d91210d391a656c22010c5753b418dd
wandecleya <wandecleya@gmail.com>
2016-05-05 17:57:02 -0300

    [Wandy] - NA: Adds sitemap script on the gulp file

--- themes/main/gulpfile.js
+++ themes/main/gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp'),
-    baseDir = __dirname +'/source'
-;
+    baseDir = __dirname +'/source';
 
 var TASKS = [
     'template',
+    'sitemap.js',
     'imagemin',
     'javascript',
     'sass',
@@ -12,7 +12,7 @@ var TASKS = [
 
 TASKS.forEach(function(name) {
     gulp.task(name, require(__dirname + '/gulp/' + name)(baseDir));
-});;
+});
 
-gulp.task('default', ['javascript', 'sass', 'imagemin', 'template', 'watch']);
-gulp.task('build', ['javascript', 'sass', 'imagemin', 'template']);
+gulp.task('default', ['javascript', 'sass', 'imagemin', 'template', 'sitemap', 'watch']);
+gulp.task('build', ['javascript', 'sass', 'imagemin', 'template', 'sitemap']);
