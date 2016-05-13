7aa1f16dc74fe831b92536a58c9aa94414cffb2a
rgusmao <rgusmao@thoughtworks.com>
2016-05-13 16:57:45 -0300

    [Patrick, Renata] - NA: git stashes _config.yml before update code to not have changes overwritten

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@ PATH=$PATH:/usr/local/node/node-default/bin
 
 pushd /home/frida/components/site
   pm2 stop frida-hexo-generate
+  git stash
   git pull
+  git stash pop
   npm install
 
   pushd ./themes/main
