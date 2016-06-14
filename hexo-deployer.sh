baff4d6fa9c9c7b42a04585ffcb668c89889ae27
Patrick Maia <patrickjourdanmaia@gmail.com>
2016-06-14 15:01:15 -0300

    [Michael, Patrick] a3CvMT73 specifies hexo's working dir

--- hexo-deployer.sh
+++ hexo-deployer.sh
@@ -4,7 +4,7 @@ SITE_HOME=$1
 SITE_DEST=$2
 HEXO=$SITE_HOME/node_modules/hexo/bin/hexo
 
-$HEXO clean
-$HEXO generate
+$HEXO clean --cwd $SITE_HOME
+$HEXO generate --cwd $SITE_HOME
 
 rsync -rtvu --delete $SITE_HOME/public/ $SITE_DEST
