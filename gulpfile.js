8ccf7db1d3046b267bbe52a1f0a52793e9d727f5
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-08-23 12:04:17 -0300

    [Thiago] GTyInmkD adds version string to main.css to prevent caching

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,9 @@
 'use strict';
 
 var gulp = require('gulp'),
-    shell = require('gulp-shell'),
-    spawn = require('child_process').spawn;
+    spawnSync = require('child_process').spawnSync;
 
 gulp.task('default', function(cb){
-  spawn('./node_modules/hexo/bin/hexo', ['serve'], { stdio: 'inherit' });
-  spawn('gulp', ['--cwd', 'themes/main/'], { stdio: 'inherit' });
+  spawnSync('./node_modules/hexo/bin/hexo', ['clean'], { stdio: 'inherit' });
+  spawnSync('gulp', ['--cwd', 'themes/main/'], { stdio: 'inherit' });
 });
