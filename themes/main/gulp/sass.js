ee4a733b2e595c7405f9c1143390e64a6d5c6091
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-08-25 11:50:50 -0300

    [Thiago] GTyInmkD undoes changes to gulpfiles related to adding version strings to main.css

--- themes/main/gulp/sass.js
+++ themes/main/gulp/sass.js
@@ -4,14 +4,11 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var nodeResetScss = require('node-reset-scss').includePath;
 var cleanCSS = require('gulp-clean-css');
-var clean = require('gulp-dest-clean');
-var gitHashShort = require('child_process').execSync('git rev-parse --short HEAD').toString().trim();
 
 module.exports =  function (baseDir) {
 	return function() {
 	  return gulp.src(['src/sass/**/*.scss', '!src/sass/_helpers/'])
 			.pipe(plumber())
-			.pipe(clean(baseDir+'/css/'))
 	    .pipe(sass({
 	        includePaths: [nodeResetScss, 'styles']
 	    }).on('error', sass.logError))
--- themes/main/scripts/git_hash_helper.js
+++
@@ -1,2 +0,0 @@
-var execSync = require('child_process').execSync;
-process.env.SHORT_GIT_HASH = execSync('git rev-parse --short HEAD').toString().trim();
