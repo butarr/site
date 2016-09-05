e338ccd65af2db6f5c18cb373c403876b2f9d6e2
rgusmao <rgusmao@thoughtworks.com>
2016-09-05 11:51:51 -0300

    Renata 27E3rYvL Changes newsletter method to use workflow tag

--- themes/main/src/js/allin.js
+++ themes/main/src/js/allin.js
@@ -1,29 +1,15 @@
-function registerOnAllIn(emailInputSelector) {
-  var reqParams = {};
-  reqParams['acao'] = 'dados';
-  reqParams['tipo'] = 'inserir_lista';
-  reqParams['dados'] = {};
-  reqParams['dados']['dfALLIN'] = '974213|7849';
-  reqParams['dados']['inputs'] = [];
-  reqParams['dados']['inputs'][0] = { dfALLIN: '974213|7849' };
-  reqParams['dados']['inputs'][1] = { redurl: '' };
-  reqParams['dados']['inputs'][2] = { valid: 1 };
-  reqParams['dados']['inputs'][3] = { nm_email: $(emailInputSelector).val() };
-  reqParams['dados']['inputs'][4] = { recebe: true };
+function registerOnAllIn(emailInputSelector){
+  __blc['id'] = "7849";
+    try {
+        lc.sendData({
+            nm_email : $(emailInputSelector).val(),
+            lista : {
+                nm_lista : "BrasilDeFato",
+                atualizar : "1",
+            }
+        });
 
-  $.ajax({
-    url: 'http://e.allin.brasildefato.com.br/embed/76af83270c68531f81f348072afb0576/post.php',
-    type: 'POST',
-    data: reqParams,
-    dataType: 'JSON',
-    success: function() {
-      console.log('sucesso');
-    },
-    error: function(jqXHR, textStatus, errorThrown) {
-      console.error("\njqXHR:" + jqXHR + "\ntextStatus:" + textStatus + "\nerrorThrown:" + errorThrown + "\n");
+    } catch (e) {
     }
-  });
-
-  // Clean input object
-  document.getElementById("emailInputMultipleColumnLayout").value = "";
+    document.getElementById("emailInputMultipleColumnLayout").value = "";
 }
