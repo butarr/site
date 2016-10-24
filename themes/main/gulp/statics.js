ade3ee2db3bd31ccf7598048e90000e6780b3fd9
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-10-24 16:52:19 -0200

    [Thiago] rz5EuAZB fixes deployment of static md files

--- themes/main/gulp/statics.js
+++ themes/main/gulp/statics.js
@@ -1,10 +1,17 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
+var YAML = require('js-yaml');
+var fs = require('fs');
 
 module.exports = function (baseDir) {
     return function() {
+      var doc = YAML.safeLoad(fs.readFileSync(baseDir+'/../../../_config.yml', 'utf8'));
+      var sourceDir = doc.source_dir;
+      if(!sourceDir.startsWith("/")) {
+        sourceDir = baseDir+'/../../../'+sourceDir;
+      }
       return gulp.src('src/mds/**/*.md')
                        .pipe(plumber())
-                       .pipe(gulp.dest(baseDir+'/../../../source'));
+                       .pipe(gulp.dest(sourceDir));
     }
 };
