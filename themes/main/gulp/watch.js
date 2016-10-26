17986692e3d06ae475e1ea2c8e1159fae43445fe
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-10-26 09:48:43 -0200

    [Thiago] rz5EuAZB adds mds to gulp watch list

--- themes/main/gulp/watch.js
+++ themes/main/gulp/watch.js
@@ -5,5 +5,6 @@ module.exports = function () {
 		gulp.watch('src/js/**/*.js', ['javascript'])
 	  gulp.watch('src/img/**/*.{jpg,png,gif}', ['imagemin'])
 	  gulp.watch('src/sass/**/*.scss', ['sass']);
+		gulp.watch('src/mds/**/*.md', ['statics']);
 	}
 };
