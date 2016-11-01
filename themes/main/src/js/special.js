c86e928ae70bcaf89bef090914533b508d397268
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-11-01 11:28:11 -0200

    [Thiago, Nicholas] AMEyOQM5 optimizes mobile share bar javascript on special

--- themes/main/src/js/special.js
+++ themes/main/src/js/special.js
@@ -1,17 +1,14 @@
 $(document).ready(function() {
-  $(window).scroll(function(){
-    var coverHeight = $('#cover').height();
-    var footerTop = document.documentElement.scrollHeight - $('#page-footer').height();
+  var coverHeight = $('#cover').height();
+  var footerTop = $(document).height() - $('#page-footer').height();
 
+  $(window).scroll(function(){
     var pastHeader = ($(window).scrollTop() > coverHeight);
     $('#share-special').toggleClass('share-special-top', !pastHeader);
     $('#share-special').toggleClass('share-special-middle', pastHeader);
 
-    var windowBottom = $(window).scrollTop()  + $(window).height();
-    if(windowBottom > footerTop) {
-      $('#share-special').toggleClass('share-special-bottom', true);
-    } else if(windowBottom < (footerTop-64)) {
-      $('#share-special').toggleClass('share-special-bottom', false);
-    }
+    var windowBottom = $(window).scrollTop() + $(window).height();
+    var atBottom = windowBottom > footerTop;
+    $('#share-special').toggleClass('share-special-bottom', atBottom);
   });
 });
