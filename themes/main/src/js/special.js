268afe39a9abdff3b0a67ff113d5b9bfaad66d58
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-11-01 17:09:57 -0200

    [Thiago] AMEyOQM5 separates social share bar behavior description into document.ready and window.load

--- themes/main/src/js/special.js
+++ themes/main/src/js/special.js
@@ -1,12 +1,17 @@
 $(document).ready(function() {
   var coverHeight = $('#cover').height();
-  var footerTop = $(document).height() - $('#page-footer').height();
 
   $(window).scroll(function(){
     var pastHeader = ($(window).scrollTop() > coverHeight);
     $('#share-special').toggleClass('share-special-top', !pastHeader);
     $('#share-special').toggleClass('share-special-middle', pastHeader);
+  });
+});
 
+$(window).load(function() {
+  var footerTop = $(document).height() - $('#page-footer').height();
+
+  $(window).scroll(function(){
     var windowBottom = $(window).scrollTop() + $(window).height();
     var atBottom = windowBottom > footerTop;
     $('#share-special').toggleClass('share-special-bottom', atBottom);
