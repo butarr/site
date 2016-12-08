1670209ac02ec59c97392b7f5aa8cc07b802568d
Giuliana <giugianam@gmail.com>
2016-12-08 18:44:15 -0200

    [giu] nKdiE2yb Adds the files app.js and valimate.json, these configures  HTML test from site

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const http = require('http');
+const notifyValimate = require('valimate-notifier');
+var Hexo = require('hexo');
+var hexo = new Hexo (process.cwd(), {});
+
+hexo.init().then(function(){
+  hexo.call('generate',{}).then(function(){
+    hexo.call('serve',{}).then(function() {
+       notifyValimate(true);
+    });
+  });
+}).catch(e => notifyValimate(false));
