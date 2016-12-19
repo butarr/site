9bb9f0f207a223a8dc9bf643cdcae12d20101194
Thiago Hersan <thiago.hersan+github@gmail.com>
2016-12-19 16:40:10 -0200

    [Thiago] xxbPBD5K trying to fix snap error when copying md files

--- themes/main/gulp/statics.js
+++ themes/main/gulp/statics.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var YAML = require('js-yaml');
 var fs = require('fs');
+var path = require('path');
 var events = require('events');
 
 //remove warning about max events listeners
@@ -9,10 +10,10 @@ events.EventEmitter.prototype._maxListeners = 20;
 
 module.exports = function (baseDir) {
     return function() {
-      var doc = YAML.safeLoad(fs.readFileSync(baseDir+'/../../../_config.yml', 'utf8'));
+      var doc = YAML.safeLoad(fs.readFileSync(path.normalize(baseDir+'/../../../_config.yml'), 'utf8'));
       var sourceDir = doc.source_dir;
       if(!sourceDir.startsWith("/")) {
-        sourceDir = baseDir+'/../../../'+sourceDir;
+        sourceDir = path.normalize(baseDir+'/../../../'+sourceDir);
       }
       return gulp.src('src/mds/**/*.md')
                        .pipe(plumber())
