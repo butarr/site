ff5fc673478e6f7ac0469a1611450bbcab592603
Bob <damindecotia@gmail.com>
2017-01-23 13:38:29 -0200

    [bob] nKdiE2yb - Run test in the environment

--- valimate-config.js
+++ valimate-config.js
@@ -2,13 +2,3 @@
 
 const http = require('http');
 const notifyValimate = require('valimate-notifier');
-var Hexo = require('hexo');
-var hexo = new Hexo(process.cwd(), {});
-
-hexo.init().then(function(){
-  hexo.call('generate',{}).then(function(){
-    hexo.call('serve',{}).then(function() {
-       notifyValimate(true);
-    });
-  });
-}).catch(e => notifyValimate(false));
