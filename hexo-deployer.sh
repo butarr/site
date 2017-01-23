54a74efdd5418bd2678a72cc0a662c0379cf077e
Bob <damindecotia@gmail.com>
2017-01-23 21:15:39 -0200

    Revert "[bob] nKdiE2yb Include validate hexo running"
    This reverts commit 1c80cf810019dd1b43aa9dc3150e66fe8b7e696d.

--- hexo-deployer.sh
+++ hexo-deployer.sh
@@ -4,10 +4,7 @@ SITE_HOME=$1
 SITE_DEST=$2
 HEXO=$SITE_HOME/node_modules/hexo/bin/hexo
 
-if [ ! -z "$(pgrep "hexo")" ] ; then
-  echo "Hexo is running!"
-else
-  $HEXO clean --cwd $SITE_HOME
-  $HEXO generate --cwd $SITE_HOME
-  rsync -rtvu --delete $SITE_HOME/public/ $SITE_DEST
-fi
+$HEXO clean --cwd $SITE_HOME
+$HEXO generate --cwd $SITE_HOME
+
+rsync -rtvu --delete $SITE_HOME/public/ $SITE_DEST
