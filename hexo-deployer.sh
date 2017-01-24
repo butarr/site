f64e49e9435c4e2e929ea941820a902174b1a0c4
Bob <damindecotia@gmail.com>
2017-01-24 14:37:21 -0200

    [bob] - Modify message and include news urls

--- hexo-deployer.sh
+++ hexo-deployer.sh
@@ -5,7 +5,7 @@ SITE_DEST=$2
 HEXO=$SITE_HOME/node_modules/hexo/bin/hexo
 
 if [ ! -z "$(pgrep "hexo")" ] ; then
-  echo "Hexo is running!"
+  echo "Hexo did not start because was already processing."
 else
   $HEXO clean --cwd $SITE_HOME
   $HEXO generate --cwd $SITE_HOME
