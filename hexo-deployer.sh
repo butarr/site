fdd17dbac4200b80dfb8e4080fe070636f520e5a
Patrick Maia <patrickjourdanmaia@gmail.com>
2017-02-02 15:03:26 -0200

    [Patrick] fKc6ol5N makes hexo clean command optional

--- hexo-deployer.sh
+++ hexo-deployer.sh
@@ -2,12 +2,15 @@
 
 SITE_HOME=$1
 SITE_DEST=$2
+SHOULD_CLEAN=$3
 HEXO=$SITE_HOME/node_modules/hexo/bin/hexo
 
 if [ ! -z "$(pgrep "hexo")" ] ; then
   echo "Hexo did not start because was already processing."
 else
-  $HEXO clean --cwd $SITE_HOME
+  if [ ${SHOULD_CLEAN:-clean} == "clean" ]; then
+    $HEXO clean --cwd $SITE_HOME
+  fi
   $HEXO generate --cwd $SITE_HOME
   rsync -rtvu --delete $SITE_HOME/public/ $SITE_DEST
 fi
